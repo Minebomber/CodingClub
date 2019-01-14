cf3cbeb4376d1defe4a730d5bed87b995f115a18
Mark Lagae <markmc909@gmail.com>
2019-01-14 12:00:06 -0800

    Basic GET requests with php

--- main.php
+++ main.php
@@ -8,11 +8,13 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
         <script src="index.js" type="text/javascript"></script>
         
-        <form action="main.php" method="get">
-            Enter your name:<br>
-            <input type="text" name="name">
-            <input type="submit" value="Submit">
-        </form>
+        <?php
+        if ($_GET["name"]) {
+            echo "Hello " . $_GET["name"];
+        }
+        ?>
         
+        <br>
+        <a href="index.php">Exit</a>
     </body>
 </html>
